056320b2f3f1721b94d2de121bf40231e8115dad
dbrian105 <oianl05@daum.net>
2024-08-24 14:13:14 +0900

    test
    예전에 만들었던거 올려봅니다. 숫자야구 개념을 활용하여 만든 유전 알고리즘 코드입니다.

---
+++ genetic algorithm.py
@@ -0,0 +1,945 @@
+import random
+
+gene = int(0)
+
+a1 = int(3)
+a2 = int(6)
+a3 = int(8)
+
+a = int(368)
+
+t1=float(0)
+t2=float(0)
+
+u1=int(0)
+u2=int(0)
+
+x1 = int(0)
+x2 = int(0)
+x3 = int(0)
+x4 = int(0)
+x5 = int(0)
+x6 = int(0)
+x7 = int(0)
+x8 = int(0)
+x9 = int(0)
+x10 = int(0)
+x11 = int(0)
+x12 = int(0)
+x13 = int(0)
+x14 = int(0)
+x15 = int(0)
+x16 = int(0)
+x17 = int(0)
+x18 = int(0)
+
+m1 = int(0)
+m2 = int(0)
+m3 = int(0)
+m4 = int(0)
+m5 = int(0)
+m6 = int(0)
+m7 = int(0)
+m8 = int(0)
+m9 = int(0)
+m10 = int(0)
+m11 = int(0)
+m12 = int(0)
+m13 = int(0)
+m14 = int(0)
+m15 = int(0)
+m16 = int(0)
+m17 = int(0)
+m18 = int(0)
+
+
+nlist = []
+
+
+list1 = []
+list2 = []
+list3 = []
+list4 = []
+list5 = []
+list6 = []
+
+
+listlist = []
+
+x1 = random.randint(0,9)    
+x2 = random.randint(0,9)
+x3 = random.randint(0,9)
+print(x1)
+print(x2)
+print(x3)
+if(x1==a1):     
+    m1=m1+100
+if(x1==a2):     
+    m1=m1+1
+if(x1==a3):     
+    m1=m1+1
+
+if(x2==a1):     
+    m2=m2+1
+if(x2==a2):     
+    m2=m2+100
+if(x2==a3):     
+    m2=m2+1
+
+if(x3==a1):     
+    m3=m3+1
+if(x3==a2):     
+    m3=m3+1
+if(x3==a3):     
+    m3=m3+100
+
+list1.append(x1)
+list1.append(x2)
+list1.append(x3)
+
+n1=m1+m2+m3+0.1
+
+print(n1)
+print(list1)
+
+
+
+
+
+x4 = random.randint(0,9)
+x5 = random.randint(0,9)
+x6 = random.randint(0,9)
+print(x4)
+print(x5)
+print(x6)
+if(x4==a1):
+    m4=m4+100
+if(x4==a2):
+    m4=m4+1
+if(x4==a3):
+    m4=m4+1
+
+if(x5==a1):
+    m5=m5+1
+if(x5==a2):
+    m5=m5+100
+if(x5==a3):
+    m5=m5+1
+
+if(x6==a1):
+    m6=m6+1
+if(x6==a2):
+    m6=m6+1
+if(x6==a3):
+    m6=m6+100
+
+list2.append(x4)
+list2.append(x5)
+list2.append(x6)
+
+n2=m4+m5+m6+0.2
+
+print(n2)
+print(list2)
+
+
+
+
+
+x7 = random.randint(0,9)
+x8 = random.randint(0,9)
+x9 = random.randint(0,9)
+print(x7)
+print(x8)
+print(x9)
+if(x7==a1):
+    m7=m7+100
+if(x7==a2):
+    m7=m7+1
+if(x7==a3):
+    m7=m7+1
+
+if(x8==a1):
+    m8=m8+1
+if(x8==a2):
+    m8=m8+100
+if(x8==a3):
+    m8=m8+1
+
+if(x9==a1):
+    m9=m9+1
+if(x9==a2):
+    m9=m9+1
+if(x9==a3):
+    m9=m9+100
+
+list3.append(x7)
+list3.append(x8)
+list3.append(x9)
+
+n3=m7+m8+m9+0.3
+
+print(n3)
+print(list3)
+
+
+
+
+
+x10 = random.randint(0,9)
+x11 = random.randint(0,9)
+x12 = random.randint(0,9)
+print(x10)
+print(x11)
+print(x12)
+if(x10==a1):
+    m10=m10+100
+if(x10==a2):
+    m10=m10+1
+if(x10==a3):
+    m10=m10+1
+
+if(x11==a1):
+    m11=m11+1
+if(x11==a2):
+    m11=m11+100
+if(x11==a3):
+    m11=m11+1
+
+if(x12==a1):
+    m12=m12+1
+if(x12==a2):
+    m12=m12+1
+if(x12==a3):
+    m12=m12+100
+
+list4.append(x10)
+list4.append(x11)
+list4.append(x12)
+
+n4=m10+m11+m12+0.4
+
+print(n4)
+print(list4)
+
+
+
+
+
+x13 = random.randint(0,9)
+x14 = random.randint(0,9)
+x15 = random.randint(0,9)
+print(x13)
+print(x14)
+print(x15)
+if(x13==a1):
+    m13=m13+100
+if(x13==a2):
+    m13=m13+1
+if(x13==a3):
+    m13=m13+1
+
+if(x14==a1):
+    m14=m14+1
+if(x14==a2):
+    m14=m14+100
+if(x14==a3):
+    m14=m14+1
+
+if(x15==a1):
+    m15=m15+1
+if(x15==a2):
+    m15=m15+1
+if(x15==a3):
+    m15=m15+100
+
+list5.append(x13)
+list5.append(x14)
+list5.append(x15)
+
+n5=m13+m14+m15+0.5
+
+print(n5)
+print(list5)
+
+x16 = random.randint(0,9)
+x17 = random.randint(0,9)
+x18 = random.randint(0,9)
+print(x16)
+print(x17)
+print(x18)
+if(x16==a1):
+    m16=m16+100
+if(x16==a2):
+    m16=m16+1
+if(x16==a3):
+    m16=m16+1
+
+if(x17==a1):
+    m17=m17+1
+if(x17==a2):
+    m17=m17+100
+if(x17==a3):
+    m17=m17+1
+
+if(x18==a1):
+    m18=m18+1
+if(x18==a2):
+    m18=m18+1
+if(x18==a3):
+    m18=m18+100
+
+list6.append(x16)
+list6.append(x17)
+list6.append(x18)
+
+n6=m16+m17+m18+0.6
+
+print(n6)
+print(list6)
+
+
+listlist.append(list1)
+listlist.append(list2)
+listlist.append(list3)
+listlist.append(list4)
+listlist.append(list5)
+listlist.append(list6)
+
+
+
+
+nlist.append(n1)
+nlist.append(n2)
+nlist.append(n3)
+nlist.append(n4)
+nlist.append(n5)
+nlist.append(n6)
+
+nlist.sort(reverse=True)
+
+print(nlist)
+print(nlist[0])
+print(nlist[1])
+
+t1=nlist[0]
+t2=nlist[1]
+
+while(t1>1):
+    t1=t1-1
+    
+t1=t1*10
+u1=int(t1)
+
+while(t2>1):
+    t2=t2-1
+
+t2=t2*10
+u2=int(t2)
+
+print(u1)
+print(u2)
+
+
+print(listlist[u1-1])
+print(listlist[u2-1])
+
+
+
+
+
+
+
+if(random.randint(0,99)<=95):
+    x1=listlist[u1-1][0]
+else:
+    x1=random.randint(0,9)
+
+if(random.randint(0,99)<=95):
+    x2=listlist[u1-1][1]
+else:
+    x2=random.randint(0,9)
+    
+if(random.randint(0,99)<=95):
+    x3=listlist[u1-1][2]
+else:
+    x3=random.randint(0,9)
+
+
+list1[0]=x1
+list1[1]=x2
+list1[2]=x3
+
+
+print(x1)
+print(x2)
+print(x3)
+print(list1)
+
+if(random.randint(0,99)<=95):
+    x4=listlist[u2-1][0]
+else:
+    x4=random.randint(0,9)
+
+if(random.randint(0,99)<=95):
+    x5=listlist[u2-1][1]
+else:
+    x5=random.randint(0,9)
+    
+if(random.randint(0,99)<=95):
+    x6=listlist[u2-1][2]
+else:
+    x6=random.randint(0,9)
+
+
+list2[0]=x4
+list2[1]=x5
+list2[2]=x6
+    
+
+print(x4)
+print(x5)
+print(x6)
+print(list2)
+
+if(random.randint(0,99)<=95):
+    x7=listlist[u1-1][0]
+else:
+    x7=random.randint(0,9)
+
+if(random.randint(0,99)<=95):
+    x8=listlist[u1-1][1]
+else:
+    x8=random.randint(0,9)
+    
+if(random.randint(0,99)<=95):
+    x9=listlist[u1-1][2]
+else:
+    x9=random.randint(0,9)
+
+
+list3[0]=x7
+list3[1]=x8
+list3[2]=x9
+    
+
+print(x7)
+print(x8)
+print(x9)
+print(list3)
+
+if(random.randint(0,99)<=95):
+    x10=listlist[u2-1][0]
+else:
+    x10=random.randint(0,9)
+
+if(random.randint(0,99)<=95):
+    x11=listlist[u2-1][1]
+else:
+    x11=random.randint(0,9)
+    
+if(random.randint(0,99)<=95):
+    x12=listlist[u2-1][2]
+else:
+    x12=random.randint(0,9)
+
+
+list4[0]=x10
+list4[1]=x11
+list4[2]=x12
+    
+
+print(x10)
+print(x11)
+print(x12)
+print(list4)
+
+if(random.randint(0,99)<=95):
+    x13=listlist[u1-1][0]
+else:
+    x13=random.randint(0,9)
+
+if(random.randint(0,99)<=95):
+    x14=listlist[u1-1][1]
+else:
+    x14=random.randint(0,9)
+    
+if(random.randint(0,99)<=95):
+    x15=listlist[u1-1][2]
+else:
+    x15=random.randint(0,9)
+
+
+
+list5[0]=x13
+list5[1]=x14
+list5[2]=x15
+    
+
+print(x13)
+print(x14)
+print(x15)
+print(list5)
+
+if(random.randint(0,99)<=95):
+    x16=listlist[u2-1][0]
+else:
+    x16=random.randint(0,9)
+
+if(random.randint(0,99)<=95):
+    x17=listlist[u2-1][1]
+else:
+    x17=random.randint(0,9)
+    
+if(random.randint(0,99)<=95):
+    x18=listlist[u2-1][2]
+else:
+    x18=random.randint(0,9)
+
+
+
+list6[0]=x16
+list6[1]=x17
+list6[2]=x18
+    
+
+print(x16)
+print(x17)
+print(x18)
+print(list6)
+
+
+
+while True :
+
+    gene=gene+1
+    
+    m1=0
+    m2=0
+    m3=0
+    m4=0
+    m5=0
+    m6=0
+    m7=0
+    m8=0
+    m9=0
+    m10=0
+    m11=0
+    m12=0
+    m13=0
+    m14=0
+    m15=0
+    m16=0
+    m17=0
+    m18=0
+
+    n1=0
+    n2=0
+    n3=0
+    n4=0
+    n5=0
+    n6=0
+
+
+
+    if(x1==a1):
+         m1=m1+100
+    if(x1==a2):
+        m1=m1+1
+    if(x1==a3):
+        m1=m1+1
+
+    if(x2==a1):
+        m2=m2+1
+    if(x2==a2):
+        m2=m2+100
+    if(x2==a3):
+        m2=m2+1
+
+    if(x3==a1):
+        m3=m3+1
+    if(x3==a2):
+        m3=m3+1
+    if(x3==a3):
+        m3=m3+100
+
+    list1[0]=x1
+    list1[1]=x2
+    list1[2]=x3
+
+    n1=m1+m2+m3+0.1
+
+    print(n1)
+    print(list1)
+
+    if(x4==a1):
+         m4=m4+100
+    if(x4==a2):
+        m4=m4+1
+    if(x4==a3):
+        m4=m4+1
+
+    if(x5==a1):
+        m5=m5+1
+    if(x5==a2):
+        m5=m5+100
+    if(x5==a3):
+        m5=m5+1
+
+    if(x6==a1):
+        m6=m6+1
+    if(x6==a2):
+        m6=m6+1
+    if(x6==a3):
+        m6=m6+100
+
+    list2[0]=x4
+    list2[1]=x5
+    list2[2]=x6
+
+    n2=m4+m5+m6+0.2
+
+    print(n2)
+    print(list2)
+
+    if(x7==a1):
+         m7=m7+100
+    if(x7==a2):
+        m7=m7+1
+    if(x7==a3):
+        m7=m7+1
+
+    if(x8==a1):
+        m8=m8+1
+    if(x8==a2):
+        m8=m8+100
+    if(x8==a3):
+        m8=m8+1
+
+    if(x9==a1):
+        m9=m9+1
+    if(x9==a2):
+        m9=m9+1
+    if(x9==a3):
+        m9=m9+100
+
+    list3[0]=x7
+    list3[1]=x8
+    list3[2]=x9
+
+    n3=m7+m8+m9+0.3
+
+    print(n3)
+    print(list3)
+
+    if(x10==a1):
+         m10=m10+100
+    if(x10==a2):
+        m10=m10+1
+    if(x10==a3):
+        m10=m10+1
+
+    if(x11==a1):
+        m11=m11+1
+    if(x11==a2):
+        m11=m11+100
+    if(x11==a3):
+        m11=m11+1
+
+    if(x12==a1):
+        m12=m12+1
+    if(x12==a2):
+        m12=m12+1
+    if(x12==a3):
+        m12=m12+100
+
+    list4[0]=x10
+    list4[1]=x11
+    list4[2]=x12
+
+    n4=m10+m11+m12+0.4
+
+    print(n4)
+    print(list4)
+
+    if(x13==a1):
+         m13=m13+100
+    if(x13==a2):
+        m13=m13+1
+    if(x13==a3):
+        m13=m13+1
+
+    if(x14==a1):
+        m14=m14+1
+    if(x14==a2):
+        m14=m14+100
+    if(x14==a3):
+        m14=m14+1
+
+    if(x15==a1):
+        m15=m15+1
+    if(x15==a2):
+        m15=m15+1
+    if(x15==a3):
+        m15=m15+100
+
+    list5[0]=x13
+    list5[1]=x14
+    list5[2]=x15
+
+    n5=m13+m14+m15+0.5
+
+    print(n5)
+    print(list5)
+
+    if(x16==a1):
+         m16=m16+100
+    if(x16==a2):
+        m16=m16+1
+    if(x16==a3):
+        m16=m16+1
+
+    if(x17==a1):
+        m17=m17+1
+    if(x17==a2):
+        m17=m17+100
+    if(x17==a3):
+        m17=m17+1
+
+    if(x18==a1):
+        m18=m18+1
+    if(x18==a2):
+        m18=m18+1
+    if(x18==a3):
+        m18=m18+100
+
+    list6[0]=x16
+    list6[1]=x17
+    list6[2]=x18
+
+    n6=m16+m17+m18+0.6
+
+    print(n6)
+    print(list6)
+
+
+    listlist[0]=list1
+    listlist[1]=list2
+    listlist[2]=list3
+    listlist[3]=list4
+    listlist[4]=list5
+    listlist[5]=list6
+
+
+
+
+    nlist[0]=n1
+    nlist[1]=n2
+    nlist[2]=n3
+    nlist[3]=n4
+    nlist[4]=n5
+    nlist[5]=n6
+
+    nlist.sort(reverse=True)
+
+    print(nlist)
+    print(nlist[0])
+    print(nlist[1])
+
+    t1=nlist[0]
+    t2=nlist[1]
+
+    while(t1>1):
+        t1=t1-1
+    
+    t1=t1*10
+    u1=int(t1)
+
+    while(t2>1):
+        t2=t2-1
+
+    t2=t2*10
+    u2=int(t2)
+
+    print(u1)
+    print(u2)
+
+
+    print(listlist[u1-1])
+    print(listlist[u2-1])
+
+
+
+    if(random.randint(0,99)<=95):
+        x1=listlist[u1-1][0]
+    else:
+        x1=random.randint(0,9)
+
+    if(random.randint(0,99)<=95):
+        x2=listlist[u1-1][1]
+    else:
+        x2=random.randint(0,9)
+    
+    if(random.randint(0,99)<=95):
+        x3=listlist[u1-1][2]
+    else:
+        x3=random.randint(0,9)
+
+    list1[0]=x1
+    list1[1]=x2
+    list1[2]=x3
+
+
+    print(x1)
+    print(x2)
+    print(x3)
+    print(list1)
+
+    if(random.randint(0,99)<=95):
+        x4=listlist[u2-1][0]
+    else:
+        x4=random.randint(0,9)
+
+    if(random.randint(0,99)<=95):
+        x5=listlist[u2-1][1]
+    else:
+        x5=random.randint(0,9)
+    
+    if(random.randint(0,99)<=95):
+        x6=listlist[u2-1][2]
+    else:
+        x6=random.randint(0,9)
+
+
+    list2[0]=x4
+    list2[1]=x5
+    list2[2]=x6
+    
+
+    print(x4)
+    print(x5)
+    print(x6)
+    print(list2)
+
+    if(random.randint(0,99)<=95):
+        x7=listlist[u1-1][0]
+    else:
+        x7=random.randint(0,9)
+
+    if(random.randint(0,99)<=95):
+        x8=listlist[u1-1][1]
+    else:
+        x8=random.randint(0,9)
+    
+    if(random.randint(0,99)<=95):
+        x9=listlist[u1-1][2]
+    else:
+        x9=random.randint(0,9)
+
+
+    list3[0]=x7
+    list3[1]=x8
+    list3[2]=x9
+    
+
+    print(x7)
+    print(x8)
+    print(x9)
+    print(list3)
+
+    if(random.randint(0,99)<=95):
+        x10=listlist[u2-1][0]
+    else:
+        x10=random.randint(0,9)
+
+    if(random.randint(0,99)<=95):
+        x11=listlist[u2-1][1]
+    else:
+        x11=random.randint(0,9)
+    
+    if(random.randint(0,99)<=95):
+        x12=listlist[u2-1][2]
+    else:
+        x12=random.randint(0,9)
+
+
+    list4[0]=x10
+    list4[1]=x11
+    list4[2]=x12
+    
+
+    print(x10)
+    print(x11)
+    print(x12)
+    print(list4)
+
+    if(random.randint(0,99)<=95):
+        x13=listlist[u1-1][0]
+    else:
+        x13=random.randint(0,9)
+
+    if(random.randint(0,99)<=95):
+        x14=listlist[u1-1][1]
+    else:
+        x14=random.randint(0,9)
+    
+    if(random.randint(0,99)<=95):
+        x15=listlist[u1-1][2]
+    else:
+        x15=random.randint(0,9)
+
+
+
+    list5[0]=x13
+    list5[1]=x14
+    list5[2]=x15
+    
+
+    print(x13)
+    print(x14)
+    print(x15)
+    print(list5)
+
+    if(random.randint(0,99)<=95):
+        x16=listlist[u2-1][0]
+    else:
+        x16=random.randint(0,9)
+
+    if(random.randint(0,99)<=95):
+        x17=listlist[u2-1][1]
+    else:
+        x17=random.randint(0,9)
+    
+    if(random.randint(0,99)<=95):
+        x18=listlist[u2-1][2]
+    else:
+        x18=random.randint(0,9)
+
+
+
+    list6[0]=x16
+    list6[1]=x17
+    list6[2]=x18
+    
+
+    print(x16)
+    print(x17)
+    print(x18)
+    print(list6)
+
+
+    print("generation :")
+    print(gene)
+
+    if n1 == 300.1:
+        break
+    if n2 == 300.2:
+        break
+    if n3 == 300.3:
+        break
+    if n4 == 300.4:
+        break
+    if n5 == 300.5:
+        break
+    if n6 == 300.6:
+        break
